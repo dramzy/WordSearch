28b79b7a0f5d4c68bfd99a983b8952a89f557d1a
dramzy <dramzy@gmu.edu>
2017-01-14 21:05:26 -0500

    Added JUnit testing

---
+++ src/test/java/com/dramzy/dictionary/DictionaryTests.java
@@ -0,0 +1,14 @@
+package com.dramzy.dictionary;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DictionaryTests {
+
+   @Test
+   public void test() {
+      fail("Not yet implemented");
+   }
+
+}
